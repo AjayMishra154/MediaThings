50153491f4c186eb35ad9f62ed1e4a2d931ea2a4
Ajay Mishra <ajaymishra4215@gmail.com>
2024-08-10 09:46:31 +0530

    favicon

--- src/components/Loading/Loading.js
+++ src/components/Loading/Loading.js
@@ -1,10 +1,16 @@
 // components/Loading.js
-import React from 'react';
-import './Loading.css'; // Import the CSS file for styling
+import React from "react";
+import "./Loading.css"; // Import the CSS file for styling
+import { Helmet } from "react-helmet";
 
 const Loading = () => {
   return (
     <>
+      <Helmet>
+        <title>MediaThings</title>
+        <meta name="description" content="This is a description of my page" />
+        <meta name="keywords" content="React, Helmet, Meta Tags" />
+      </Helmet>
       <div id="loading-container">
         <img src="/logo1.png" alt="Loading..." id="loading-logo" />
 
